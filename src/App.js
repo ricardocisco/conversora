83b7b4b67fafef4098c9fe38562a14b11b55c44f
ricardocisco <ricardopenga70@gmail.com>
2024-01-17 22:46:09 -0300

    update

--- src/App.js
+++ src/App.js
@@ -22,28 +22,28 @@ function App() {
     <div className="box">
       <h1>Conversor de Moeda</h1>
       <div className="box-input">
-        <label>{deMoeda} </label>
-        <input 
-        type="number" 
-        value={value}
-        onChange={(e) => setValue(e.target.value)}
-        />
+          <label>{deMoeda}</label>
+          <input 
+          value={value}
+          onChange={(e) => setValue(e.target.value)}
+          />
+          <select value={deMoeda} onChange={(e) => setDeMoeda(e.target.value)}>
+            <option value="USD">Dolar</option>
+            <option value="BRL">Reais</option>
+            <option value="CNY">Yuan</option>
+          </select>
       </div>
-      <div className="box-select">
-        <select value={deMoeda} onChange={(e) => setDeMoeda(e.target.value)}>
-          <option value="USD">Dolar</option>
-          <option value="BRL">Reais</option>
-          <option value="CNY">Yuan</option>
-        </select>
-        <select value={paraMoeda} onChange={(e) => setParaMoeda(e.target.value)}>
-          <option value="USD">Dolar</option>
-          <option value="BRL">Reais</option>
-          <option value="CNY">Yuan</option>
-        </select>
-      </div>
+      <div className="box-input">
+          <label>{paraMoeda}</label>    
+          <input value={resultado.toFixed(2)}/>
+          <select value={paraMoeda} onChange={(e) => setParaMoeda(e.target.value)}>
+            <option value="USD">Dolar</option>
+            <option value="BRL">Reais</option>
+            <option value="CNY">Yuan</option>
+          </select>
+      </div> 
       <div className="box-result">
         <button onClick={() => converterMoeda()}>Converter</button>
-        <p>{paraMoeda} {resultado.toFixed(2)}</p>     
       </div>
     </div>
   );
